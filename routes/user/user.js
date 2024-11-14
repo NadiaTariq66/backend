7c3e099f58df94442b676f3bd1d407d0e321c6f1
NadiaTariq66 <128166024+NadiaTariq66@users.noreply.github.com>
2024-11-14 19:15:40 +0500

    user route

--- routes/user/user.js
+++ routes/user/user.js
@@ -3,6 +3,6 @@ const multer =require("multer");
 const userController = require("../../controllers/userController");
 const router = express.Router();
 
-router.post("/user/register", userController.userRegister);
+router.post("/user/userRegister", userController.userRegister);
 
 module.exports = router;
